4bb59d31523a9832def16c064366fdff21b2d3bb
a87146423 <51416119+StephannSepp@users.noreply.github.com>
2025-05-17 11:33:03 +0800

    styled javascripts

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,16 +1,29 @@
-document.querySelectorAll('[data-dialog-toggle]').forEach((el) => {
-  el.addEventListener('click', (ev) => {
-    const target = document.getElementById(ev.currentTarget.dataset['dialogToggle']);
-    if (target !== null && target.tagName === 'DIALOG') {
-      if (target.open) {
-        target.close();
-      } else {
-        target.showModal();
+const setupAllModalDialogs = () => {
+  document.querySelectorAll('[data-dialog-toggle]').forEach((el) => {
+    el.addEventListener('click', (ev) => {
+      const target = document.getElementById(
+        ev.currentTarget.dataset['dialogToggle']
+      );
+      if (target !== null && target.tagName === 'DIALOG') {
+        if (target.open) {
+          target.close();
+        } else {
+          target.showModal();
+        }
       }
-    }
+    });
   });
-});
+};
+
+const setupCopyrightFooter = () => {
+  const copyrightFooter = document.getElementById('copyright-footer');
+  if (copyrightFooter === null) {
+    return;
+  }
+  copyrightFooter.innerText = `© ${new Date().getFullYear()} 尾矢すみ💙`;
+};
 
 (() => {
-  document.getElementById('copyright-footer').innerText = `© ${new Date().getFullYear()} 尾矢すみ💙`
+  setupAllModalDialogs();
+  setupCopyrightFooter();
 })();
